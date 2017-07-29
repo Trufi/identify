75c4c2df7638c18eda8909e18bd9a1757146bf83
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-07-30 00:03:30 +0700

    Add waves

--- src/cube.js
+++ src/cube.js
@@ -2,11 +2,11 @@ import { mat4 } from 'gl-matrix';
 
 const key = ({x, y, z}) => `${x}_${y}_${z}`;
 
-export const BYTES_PER_VERTEX = 11 * 4;
+export const BYTES_PER_VERTEX = 12 * 4;
 export const VERTICES_PER_CUBE = 36;
 
 export default class Cube {
-    constructor(position) {
+    constructor(position, shift) {
         this.position = position;
         this.size = [1, 1, 1];
         this.color = [1, 0, 0, 1];
@@ -16,6 +16,7 @@ export default class Cube {
             yz: [0, 0, 1, 1]
         };
         this.uniqColor = [1, 1, 1, 1];
+        this.shift = shift;
         this.buffer = this._generateBuffer();
     }
 
@@ -67,10 +68,11 @@ export default class Cube {
         return [
             (x + this.position[0]) * this.size[0],
             (y + this.position[1]) * this.size[1],
-            (z + this.position[2]) * this.size[2]
+            (z + this.position[2]) * this.size[2],
         ].concat(
             faceColor,
-            this.uniqColor
+            this.uniqColor,
+            this.shift,
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import { mat4 } from 'gl-matrix';
 import Cube, { BYTES_PER_VERTEX, VERTICES_PER_CUBE } from './cube';
+var compileShader = WebGLRenderingContext.prototype.compileShader;
 
+WebGLRenderingContext.prototype.compileShader = function(shader) {
+    compileShader.call(this, shader);
+
+    if (!this.getShaderParameter(shader, this.COMPILE_STATUS)) {
+        console.log(this.getShaderInfoLog(shader));
+    }
+};
+
+var linkProgram = WebGLRenderingContext.prototype.linkProgram;
+WebGLRenderingContext.prototype.linkProgram = function(program) {
+    linkProgram.call(this, program);
+
+    if (!this.getProgramParameter(program, this.LINK_STATUS)) {
+        console.error(this.getProgramInfoLog(program));
+    }
+};
 const vertexShaderCode = `
     attribute vec3 a_position;
     attribute vec3 a_color;
+    attribute float a_shift;
     uniform mat4 u_camera;
+    uniform float u_time;
     varying vec3 v_color;
     void main(void) {
         v_color = a_color;
-        gl_Position = u_camera * vec4(a_position, 1.0);
+        gl_Position = u_camera * vec4(a_position.xy, a_position.z + sin((a_shift + u_time / 50.) / 3.0) * 3.0, 1.0);
     }
 `;
 
@@ -46,18 +65,20 @@ gl.linkProgram(program);
 
 // Получим местоположение переменных в программе шейдеров
 const uCamera = gl.getUniformLocation(program, 'u_camera');
+const uTime = gl.getUniformLocation(program, 'u_time');
 const aPosition = gl.getAttribLocation(program, 'a_position');
 const aColor = gl.getAttribLocation(program, 'a_color');
+const aShift = gl.getAttribLocation(program, 'a_shift');
 
 const cameraMatrix = mat4.create();
 mat4.perspective(cameraMatrix, 45, window.innerWidth / window.innerHeight, 0.1, 1000);
 const lookAtMatrix = mat4.create();
 
 const zoom = 50;
-mat4.lookAt(lookAtMatrix, [-zoom, -zoom, zoom], [0, 0, 0], [0, 0, 1]);
+mat4.lookAt(lookAtMatrix, [-100, 90, 30], [-50, 50, 0], [0, 0, 1]);
 mat4.mul(cameraMatrix, cameraMatrix, lookAtMatrix);
 
-const k = 20;
+const k = 40;
 const padding = 5;
 const middle = k / 2;
 const cubes = [];
@@ -67,7 +88,7 @@ for (let i = 0; i < k; i++) {
             (i - middle) * (1 + padding),
             (j - middle) * (1 + padding),
             0
-        ]);
+        ], i + j);
         cubes.push(cube);
     }
 }
@@ -83,6 +104,9 @@ function batching(cubes) {
 const glBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, batching(cubes), gl.STATIC_DRAW);
+
+const startTime = Date.now();
+// let lastRenderTime = Date.now();
 
 function render() {
     // Запрашиваем рендеринг на следующий кадр
@@ -105,11 +129,14 @@ function render() {
     gl.enableVertexAttribArray(aPosition);
     gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, BYTES_PER_VERTEX, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
     gl.enableVertexAttribArray(aColor);
     gl.vertexAttribPointer(aColor, 4, gl.FLOAT, false, BYTES_PER_VERTEX, 3 * 4);
 
+    gl.enableVertexAttribArray(aShift);
+    gl.vertexAttribPointer(aShift, 1, gl.FLOAT, false, BYTES_PER_VERTEX, 11 * 4);
+
     gl.uniformMatrix4fv(uCamera, false, cameraMatrix);
+    gl.uniform1f(uTime, time - startTime);
 
     gl.drawArrays(gl.TRIANGLES, 0, VERTICES_PER_CUBE * cubes.length);
 
